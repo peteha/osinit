729d17a279b39eaf7ad5972e9d0b316a0a862245
peteha <peter@pggb.net>
2023-01-15 20:11:38 +1000

    ss5

--- init.sh
+++ init.sh
@@ -84,3 +84,147 @@ if [[ $raspi == "True" ]]; then
         fi
 fi
 echo
+if [[ "$gitpk" == "True" ]]
+    then
+        echo "## Getting SSH Keys ##"
+		gitpk_dl=`curl -s https://github.com/$username.keys`
+        if [[ $gitpk_dl != "Not Found" ]]
+        then
+            if grep -Fxq "$gitpk_dl" /home/$username/.ssh/authorized_keys
+                then
+                    echo "## Already in authorized_keys ##"
+                else
+                    echo "Adding authorized_keys for $username"
+                    sudo mkdir -p /home/$username/.ssh
+                    sudo echo "$gitpk_dl" >> /home/$username/.ssh/authorized_keys
+                    sudo chown $username:$username /home/$username/.ssh/authorized_keys
+            fi
+        else
+            echo "## No Keys in GitHub for $username ##"
+        fi
+fi
+echo
+echo
+echo "Username will be:             $username"
+## Hostname Setup
+if [[ "$dietpi" == "False" ]]
+then
+    if [[ "$buildhostname" != "$HOSTNAME" ]]
+        then
+            echo "## Setting Hostname $buildhostname ##"
+	        sudo hostnamectl set-hostname $buildhostname
+        else
+            buildhostname=$"$HOSTNAME"
+    fi
+    echo "Hostname will be:             $buildhostname"
+    sed -i.bak "/buildhostname=/c\buildhostname=$buildhostname" hostbuild.env && rm hostbuild.env.bak
+
+    if [[ "$cur_tz" != "$tz" ]]
+        then
+            echo "## Setting Timezone $tz ##"
+	        sudo timedatectl set-timezone $tz
+        else
+            tz=$"$cur_tz"
+    fi
+    echo "Timezone will be:             $tz"
+fi
+
+if [[ "$k8boot" == "True" ]]
+    then
+        if [ -f "$bootfile" ]; then
+            if grep -q "$k8_params" $bootfile; then
+                echo "## Params for K8 already in $bootfile"
+            else
+                printf %s "$k8_params" >> $bootfile
+                echo "## Params for K8 added to $bootfile"
+            fi
+        else
+            echo "## Bootfile not found - $bootfile ##"
+        fi
+fi
+echo
+if [[ $createcert == "True" ]]
+then
+    if [ ! -f /home/$username/cfcred/cf-api-token.ini ]
+    then
+        echo -n "Enter CloudFlare API Token: "
+        read cfapitoken
+        mkdir -p /home/$username/cfcred
+		echo dns_cloudflare_api_token = "$cfapitoken" > /home/$username/cfcred/cf-api-token.ini
+		chmod 600 /home/$username/cfcred/cf-api-token.ini
+    fi
+    if ! command -v certbot &> /dev/null; then
+        echo "## No certbot installed ##"
+        exit
+    fi
+    echo
+    echo "## Certbot and modules installed ##"
+	if [ ! -z ${buildhostname} ]
+	then
+		echo "## Creating Key for Host $buildhostname ##"
+        ssl_admin=$"$ssl_admin_pre$domain"
+		sudo certbot certonly --dns-cloudflare --dns-cloudflare-credentials /home/$username/cfcred/cf-api-token.ini -d $fullhn -m $ssl_admin --agree-tos -n
+        if [ -f /etc/letsencrypt/live/$fullhn/fullchain.pem ]
+        then
+            echo "Copying certs for $fullhn"
+            mkdir -p $certdir
+            bash -c "cat /etc/letsencrypt/live/$fullhn/fullchain.pem /etc/letsencrypt/live/$fullhn/privkey.pem >$certdir/$fullhn.cert"
+            bash -c "cat /etc/letsencrypt/live/$fullhn/fullchain.pem >$certdir/$fullhn-fullchain.cert"
+            bash -c "cat /etc/letsencrypt/live/$fullhn/privkey.pem >$certdir/$fullhn-privkey.key"
+            chown -R $username:$username $certdir
+        fi
+    fi
+fi
+echo
+
+# Install Cockpit #
+cockpitstatus=$(systemctl is-active cockpit.socket)
+if [[ $inst_cockpit == "True" ]]; then
+    echo "## Setting up Cockpit ##"
+    if [ -f /etc/letsencrypt/live/$fullhn/fullchain.pem ]; then
+            echo "Copying certs for Cockpit"
+            sudo bash -c "cat /etc/letsencrypt/live/$fullhn/fullchain.pem /etc/letsencrypt/live/$fullhn/privkey.pem >/etc/cockpit/ws-certs.d/$fullhn.cert"
+            sudo systemctl stop cockpit.service
+            sudo systemctl start cockpit.service
+    fi
+    echo
+    echo "## Cockpit is installed and running ##"
+fi
+
+# Install Docker
+if [[ $inst_docker == "True" ]]; then
+    if [[ $(which docker) && $(docker --version) ]]; then
+        echo "## Docker installed ##"
+    else
+        echo "## Installing Docker ##"
+        curl -sSL https://get.docker.com | sh
+        groupadd docker
+        usermod -aG docker $username
+    fi
+    if [[ $inst_dockercompose == "True" ]]; then
+        pip3 install docker-compose
+    fi
+fi
+
+# Install NTP #
+ntpstatus=$(systemctl is-active ntp)
+if [[ $inst_ntp == "True" ]]; then
+    sed -i '/^pool /d' /etc/ntp.conf
+    echo "pool $ntpserver" >> /etc/ntp.conf
+    systemctl restart ntp
+    echo
+    echo "## NTP Installed and using $ntpserver ##"
+fi
+
+
+if [[ $update == "True" ]]; then
+    echo
+    echo "## Updating environment and installing packages ##"
+	sudo apt update
+	sudo apt upgrade -y
+fi
+
+if [[ $reboot == "True" ]]
+then
+    reboot
+fi
