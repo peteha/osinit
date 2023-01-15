758ceeba9cf7912fd38944ecd48482e98e69b145
peteha <peter@pggb.net>
2023-01-15 20:24:20 +1000

    ss12

--- init.sh
+++ init.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 echo "### Init Process Starting ###"
 echo "## Setting Up OS Build Directory ##"
-# Create Base DIR
-mkdir -p /opt/osbuild
-cd /opt/osbuild
 ##
-echo "## Installing Scripts ##"
-curl -fs https://raw.githubusercontent.com/peteha/osinit/main/osinit.sh --output osinit.sh
-chmod +x osinit.sh
-curl -fs https://raw.githubusercontent.com/peteha/osinit/main/certbuild.sh --output certbuild.sh
-chmod +x certbuild.sh
-##
-if [ ! -f hostbuild.env ]; then
-    echo "## No hostbuild.env file available ##"
-    curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/hostbuild.env --output hostbuild.env
-    nano hostbuild.env
-fi
 echo "## Scripts Installed - remove /opt/osbuild/hostbuild.env to reset ##"
 echo "## Using hostbuild.env ##"
 source hostbuild.env
