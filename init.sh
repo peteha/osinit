811f51e71f69ea3ef81d05cb0ece01cbdb47547b
peteha <peter@pggb.net>
2023-01-15 19:54:15 +1000

    ss

--- init.sh
+++ init.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-echo "## Setting Up OS Build Directory ##"
-# Create Base DIR
-mkdir -p /opt/osbuild
-cd /opt/osbuild
-##
-##
-echo "## Installing Scripts ##"
-curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/osinit.sh --output osinit.sh
-chmod +x osinit.sh
-curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/certbuild.sh --output certbuild.sh
-chmod +x certbuild.sh
-##
-##
+echo "### Init Process Starting ###"
