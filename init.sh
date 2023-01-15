f8c36587dfb32fe16c2b9376de1937e74c472fa2
peteha <peter@pggb.net>
2023-01-15 19:56:38 +1000

    ss

--- init.sh
+++ init.sh
@@ -5,3 +5,14 @@ echo "## Setting Up OS Build Directory ##"
 mkdir -p /opt/osbuild
 cd /opt/osbuild
 ##
+echo "## Installing Scripts ##"
+curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/osinit.sh --output osinit.sh
+chmod +x osinit.sh
+curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/certbuild.sh --output certbuild.sh
+chmod +x certbuild.sh
+##
+if [ ! -f hostbuild.env ]; then
+    echo "## No hostbuild.env file available ##"
+    curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/hostbuild.env --output hostbuild.env
+    nano hostbuild.env
+fi
